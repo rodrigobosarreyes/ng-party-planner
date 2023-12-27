15c869c5f6c0bf7493a1730f845df0115936db87
Rodrigo Bosarreyes <RodrigoBosarreyes@outlook.es>
2023-12-27 15:01:21 +0100

    using state

--- src/app/features/guests/components/guests/guests.component.ts
+++ src/app/features/guests/components/guests/guests.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromGuests from '../../../../core/stores/guest';
 
 @Component({
   selector: 'app-guests',
   standalone: true,
   imports: [],
   templateUrl: './guests.component.html',
-  styleUrl: './guests.component.scss'
+  styleUrl: './guests.component.scss',
 })
-export class GuestsComponent {
+export class GuestsComponent implements OnInit {
+  guests$ = this.store.select(fromGuests.selectAllGuests);
+  defaultGuests: ReadonlyArray<fromGuests.Guest> = [
+    {
+      id: crypto.randomUUID(),
+      cellPhone: '1',
+      email: 'email',
+      fullName: 'Full Name',
+      isAttendeeConfirmed: null,
+      picture: 'pic',
+    },
+  ];
 
+  constructor(private readonly store: Store) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(
+      fromGuests.GuestsActions.loadGuests({ guests: this.defaultGuests })
+    );
+  }
 }

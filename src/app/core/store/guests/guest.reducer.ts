950826aa5bf62af43a92ce72a733db46051fe55b
Rodrigo Bosarreyes <RodrigoBosarreyes@outlook.es>
2024-01-06 02:48:51 +0100

    Add reductors

--- src/app/core/store/guests/guest.reducer.ts
+++ src/app/core/store/guests/guest.reducer.ts
@@ -9,5 +9,23 @@ export const guestsReducer = createReducer(
   on(
     GuestActions.guestsLoaded,
     (_state, { guests }): Array<Guest> => [...guests]
+  ),
+  on(
+    GuestActions.guestAdded,
+    (state, guest): Array<Guest> => [...state, guest]
+  ),
+  on(
+    GuestActions.guestRemoved,
+    (state, { id }): Array<Guest> => state.filter((g) => g.id !== id)
+  ),
+  on(
+    GuestActions.attendenceConfirmed,
+    (state, { id }): Array<Guest> =>
+      state.map((g) => (g.id === id ? { ...g, isAttendeeConfirmed: true } : g))
+  ),
+  on(
+    GuestActions.attendenceDeclined,
+    (state, { id }): Array<Guest> =>
+      state.map((g) => (g.id === id ? { ...g, isAttendeeConfirmed: false } : g))
   )
 );

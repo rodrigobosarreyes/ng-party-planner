b9c04e811843862f418e4547735b2d9ea27d0ce5
Rodrigo Bosarreyes <RodrigoBosarreyes@outlook.es>
2024-01-07 00:23:55 +0100

    New selectors

--- src/app/core/store/guests/guest.selectors.ts
+++ src/app/core/store/guests/guest.selectors.ts
@@ -4,3 +4,18 @@ import { createSelector } from '@ngrx/store';
 export const selectAllGuests = (state: AppState) => state.guests;
 
 export const selectGuests = createSelector(selectAllGuests, (g) => g);
+
+export const selectConfirmedGuests = createSelector(
+  selectAllGuests,
+  (guest) => guest?.filter((g) => g.isAttendeeConfirmed === true)
+);
+
+export const selectRejectedGuests = createSelector(
+  selectAllGuests,
+  (guest) => guest?.filter((g) => g.isAttendeeConfirmed === false)
+);
+
+export const selectUnknownGuests = createSelector(
+  selectAllGuests,
+  (guest) => guest?.filter((g) => g.isAttendeeConfirmed === null)
+);

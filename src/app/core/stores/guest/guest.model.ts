f9c88cc3e4553fdd2ba4e56bd4d7cc611d458f82
Rodrigo Bosarreyes <RodrigoBosarreyes@outlook.es>
2024-01-29 07:02:42 +0100

    remove unused stuff

--- src/app/core/stores/guest/guest.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Guest {
-  id: string;
-  fullName: string;
-  email: string;
-  cellPhone: string;
-  picture: string;
-  isAttendeeConfirmed: boolean | null;
-}
--- src/app/core/stores/guest/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './guest.reducers';
-export * from './guest.actions';
-export * from './guest.selector';
-export { Guest } from './guest.model';
